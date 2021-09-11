5f9a07de4df250eb708d5b86e7665c79cc60cdf7
hannahmilam <Hannah.milam16@gmail.com>
2021-09-10 23:08:54 -0600

    Finished Clock Stretch Goals!

--- app/Controllers/ClockController.js
+++ app/Controllers/ClockController.js
@@ -3,19 +3,41 @@ import { ProxyState } from "../AppState.js";
   function _drawClock(){
     const today = new Date()
     let h = today.getHours()
-    let m = today.getMinutes()
-    // let s = today.getSeconds()
-    document.getElementById('clock').innerHTML =  h + ":" + m;
+    let m = (today.getMinutes() < 10? '0' : '') + today.getMinutes();
+    let s = today.getSeconds()
     
+    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
+
+
     // grossly long ternary... BUT I FINALLY FIGURED IT OUT!!!!!!!!!! :D
     document.getElementById('greeting').innerHTML = today.getHours() >= 5 && today.getHours() <= 11 ? 'Good Morning' : today.getHours() >= 12 && today.getHours() <= 17 ? 'Good Afternoon' : today.getHours() >= 22 && today.getHours() <= 24 ? 'Good Evening' : 'Good Night'
   }
 
+  function _drawTwelveHourClock(){
+    const today = new Date()
+    let h = today.getHours()
+    let m = (today.getMinutes() < 10? '0' : '') + today.getMinutes();
+    let AmOrPm = h >= 12 ? 'PM' : 'AM';
+    h = (h % 12) || 12;
+    document.getElementById('twelve-hour-clock').innerHTML = h + ":" + m + " " + AmOrPm;
+  }  
+  
 export class ClockController{
 
   constructor(){
     setInterval(_drawClock, 1000)
+    setInterval(_drawTwelveHourClock, 1000)
   }
+
+  toggleClocks(){
+      document.getElementById('clock').classList.toggle('visually-hidden')
+      document.getElementById('twelve-hour-clock').classList.toggle('visually-hidden')
+  }
+}
+
+
+
+
 
   // REFERENCED AFTERNOON CHALLENGE TO UPDATE GREETING:
   // timeOfDayGreeting(hour) {
@@ -31,4 +53,3 @@ export class ClockController{
   //       return "good night"
   //   }
   //   }
-}
