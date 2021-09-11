5704d1c53a3ba96ef0c8a8cbc04441e61550a446
hannahmilam <Hannah.milam16@gmail.com>
2021-09-11 00:03:48 -0600

    added weather icons

--- app/Models/Weather.js
+++ app/Models/Weather.js
@@ -1,24 +1,11 @@
 export class Weather{
   constructor(weatherData){
 this.weather = weatherData.weather
-// will need to do a helper template to iterate through (weather)
-this.base = weatherData.base 
 this.kelvin = weatherData.main.temp
 this.f = Math.floor((this.kelvin -273.15)*1.8)+32
 this.c = Math.floor(this.kelvin-273.15)
-// will need a helper temp to iterate through all main temps...(main)
-this.visibility = weatherData.visibility 
-this.windSpeed = weatherData.wind.speed 
-this.windDeg = weatherData.wind.deg 
-this.rain = weatherData.rain 
-this.clouds = weatherData.clouds
-this.dt = weatherData.dt 
-this.sys = weatherData.sys 
-// will need helper temp to iterate through all main temps (sys)
-this.timezone = weatherData.timezone
 this.id = weatherData.id 
 this.name = weatherData.name 
-this.cod = weatherData.cod 
   }
 
   get Template(){
@@ -26,7 +13,8 @@ this.cod = weatherData.cod
     <div class="card text-light px-2 py-3 text-center my-3">
     <h3 class="selectable visually-hidden" id="f" onclick="app.weatherController.toggleWeather()">Fahrenheit: ${this.f}&#8457;</h3>
     <h3 class="visually-hidden selectable" id="c" onclick="app.weatherController.toggleWeather()">Celsius: ${this.c}&#8451;</h3>
-    <h5>${this.name} ${this.Weather}</h5>
+    <h5>${this.Weather}</h5>
+    <h5>${this.name} </h5>
     </div>
     `
   }
@@ -34,7 +22,7 @@ this.cod = weatherData.cod
   get Weather(){
     let template = ''
     let elemWeather = this.weather
-    elemWeather.forEach(w => template += `<span> - ${w.main}</span>`)
+    elemWeather.forEach(w => template += `<span>${w.description} <img src="https://openweathermap.org/img/wn/${w.icon}.png" alt="weather icon"></span>`)
     return template
 
   }
